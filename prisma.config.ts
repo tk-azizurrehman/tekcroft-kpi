40f28a137a01567533d851eb6cdbe79cd07cee78
Aziz <aziz.ur.rehman@tekcroft.com>
2026-03-10 12:14:14 +0500

    1 change in prisma config

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,4 +1,5 @@
 import "dotenv/config";
+import path from "node:path";
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
@@ -6,6 +7,7 @@ export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
+    seed: `npx tsx ${path.join("prisma", "seed.ts")}`,
   },
   datasource: {
     url: process.env.DATABASE_URL!,
